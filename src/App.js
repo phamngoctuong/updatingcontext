afb277fa392ef648cf80b136605d798e3e5a3199
Lionel <phamngoctuong1805@gmail.com>
2020-02-09 14:03:59 +0700

    Create theme.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import React, { Component } from 'react';
+import themes from './themes';
 const style = {
   fontFamily: 'sans-serif',
   textAlign: 'center'
-};
-const button = {
-  color: '#ffffff', 
-  backgroundColor: '#222222',
-  border: 'none',
-  borderRadius: '10px',
-  fontSize: '1em',
-  padding: '10px',
-  marginRight: '10px'
 };
 class App extends Component {
   render() {
     return (
       <div className="wrap" style={style}>
-        <button style={button}>Toggle Theme</button>
-        <button style={button}>Do Nothing</button>
+        <button style={themes.dark}>Toggle Theme</button>
+        <button style={themes.dark}>Do Nothing</button>
       </div>
     );
   }
---
+++ src/themes.js
@@ -0,0 +1,20 @@
+export default {
+  light: {
+    color: '#ffffff',
+    background: '#222222',
+    border: 'none',
+    borderRadius: '10px',
+	  fontSize: '1em',
+	  padding: '10px',
+	  marginRight: '10px'
+  },
+  dark: {
+    color: '#000000',
+    background: '#fff000',
+    border: 'none',
+    borderRadius: '10px',
+	  fontSize: '1em',
+	  padding: '10px',
+	  marginRight: '10px'
+  }
+};
